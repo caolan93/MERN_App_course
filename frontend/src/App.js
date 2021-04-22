b292407c49d5958eaff72e271b67b0d4396b457b
Caolan Fanning <caolan.fanning@gmail.com>
2021-04-22 12:48:23 +0100

    setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
+import React from 'react';
+import { Container } from 'react-bootstrap'
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 
 function App() {
   return (
     <>
-      <h1>Welcome To ProShop</h1>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome To ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
