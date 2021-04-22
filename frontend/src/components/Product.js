52fc7973a6cbfbf4d6df9de25a9c1b6cc2f54dd6
Caolan Fanning <caolan.fanning@gmail.com>
2021-04-22 16:52:02 +0100

    LinkContainers

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import Rating from './Rating'
 
 
 const Product = ({ product }) => {
     return (
         <Card className='my-3 p-3 rounded'>
-            <a href={`/product/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Img src={product.image} alt="product" variant='top' />
-            </a>
+            </Link>
             <Card.Body>
-                <Card.Title as='div'>
-                    <strong>{product.name}</strong>
-                </Card.Title>
+                <Link to={`/product/${product._id}`} >
+                    <Card.Title as='div'>
+                        <strong>{product.name}</strong>
+                    </Card.Title>
+                </Link>
                 <Card.Text as='div'>
                     <Rating value={product.rating} text={`${product.numReviews} reviews`} />
                 </Card.Text>
