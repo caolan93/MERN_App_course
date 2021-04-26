fd77c9cea7ce7ac3ea039d15da44492e3a80a4c4
Caolan Fanning <caolan.fanning@gmail.com>
2021-04-26 17:14:56 +0100

    redux

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,19 @@ const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} port ${PORT}`.yellow.underline.bold))
 
+
+
 app.get('/', (req, res) => {
     res.send('API is running')
 })
 
 app.use('/api/products', productRoutes)
 
+app.use((err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    res.status(statusCode)
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+    })
+})
