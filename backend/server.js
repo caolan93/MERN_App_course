1d76d7e7c1127063efa49755b6881f0482a2999b
Caolan Fanning <caolan.fanning@gmail.com>
2021-04-29 18:04:16 +0100

    back end auth

--- backend/server.js
+++ backend/server.js
@@ -4,28 +4,27 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 connectDB();
 const app = express()
 
+app.use(express.json())
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} port ${PORT}`.yellow.underline.bold))
-
-
 
 app.get('/', (req, res) => {
     res.send('API is running')
 })
 
+app.use('/api/users', userRoutes)
+
 app.use('/api/products', productRoutes)
 
-// app.use((err, req, res, next) => {
-//     const statusCode = res.statusCode === 200 ? 500 : res.statusCode
-//     res.status(statusCode)
-//     res.json({
-//         message: err.message,
-//         stack: process.env.NODE_ENV === 'production' ? null : err.stack
-//     })
-// })
+app.use(notFound)
+
+app.use(errorHandler)
