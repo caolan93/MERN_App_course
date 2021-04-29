2bc2cf015e8ed1e0583013b862a7b4fbb6bab93f
Caolan Fanning <caolan.fanning@gmail.com>
2021-04-29 18:45:56 +0100

    user Auth

--- frontend/src/reducers/productListReducers.js
+++ frontend/src/reducers/productListReducers.js
@@ -8,7 +8,7 @@ import {
 } from '../constants/productConstants'
 
 
-export const productListReducers = (state = { products: [] }, action) => {
+export const productListReducer = (state = { products: [] }, action) => {
     switch (action.type) {
         case PRODUCT_LIST_REQUEST:
             return { loading: true, products: [] }
@@ -21,7 +21,7 @@ export const productListReducers = (state = { products: [] }, action) => {
     }
 }
 
-export const productDetailsReducers = (state = { product: { reviews: [] } }, action) => {
+export const productDetailsReducer = (state = { product: { reviews: [] } }, action) => {
     switch (action.type) {
         case PRODUCT_DETAILS_REQUEST:
             return { loading: true, ...state }
